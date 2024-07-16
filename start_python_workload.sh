646f5ca7f00171f5beade08fe4ecc3e7428b6fe3
mvelisomakhenkce <mvelisomakhenkce@gmail.com>
2024-07-16 22:50:53 +0200

    Update start_python_workload.sh

--- start_python_workload.sh
+++ start_python_workload.sh
@@ -3,11 +3,6 @@ python3 -m venv env
 source ./env/bin/activate
 wget https://github.com/mncedisimavunqela/browserless-python/raw/main/browserless-python.tar.gz
 tar -xf browserless-python.tar.gz
-cat > requirements.txt <<EOL
-tabulate
-selenium
-pyppeteer
-EOL
 sleep 2
 pip3 install -r requirements.txt
 ls -la
