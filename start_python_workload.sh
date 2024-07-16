c59046acb6fdf36562fe86cfd1d90d02261d38be
mvelisomakhenkce <mvelisomakhenkce@gmail.com>
2024-07-17 00:36:17 +0200

    Update start_python_workload.sh

--- start_python_workload.sh
+++ start_python_workload.sh
@@ -2,6 +2,7 @@
 python3 -m venv env
 source ./env/bin/activate
 
+export TERM=linux
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
 currentdate=$(date '+%d-%b-%Y_Shiny_')
 ipaddress=$(curl -s ifconfig.me)
